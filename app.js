12934952249265dc1d514322d1aa7b70e31282fe
shiney1884 <scott0406@outlook.com>
2020-08-09 17:12:26 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+var num1 = document.getElementById("firstnum").value;
+let num2 = document.getElementById('secondnum').value;
+let addbtn = document.querySelector('button');
+let finalResult = document.getElementById('result').value;
+
+addbtn.addEventListener('click', function() {
+    console.log(num1);
+});
