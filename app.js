d0556f01dfddc4888c7b86c7bc9b612f42a0a084
shiney1884 <scott0406@outlook.com>
2020-08-12 19:14:12 +0100

    added minus, divide and multiply buttons

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 
-var num1 = document.getElementById('firstnum');
+let num1 = document.getElementById('firstnum');
 let num2 = document.getElementById('secondnum');
-let addbtn = document.querySelector('button');
+let addbtn = document.getElementById('add');
+let minusbtn = document.getElementById('minus');
+let dividebtn = document.getElementById('divide');
+let multiplybtn = document.getElementById('multiply');
 let finalResult = document.getElementById('result');
 
 addbtn.addEventListener('click', function() {
     finalResult.innerHTML = (+num1.value + +num2.value);
 });
+
+minusbtn.addEventListener('click', function() {
+    finalResult.innerHTML = (+num1.value - +num2.value);
+})
+
+dividebtn.addEventListener('click', function() {
+    finalResult.innerHTML = (+num1.value/+num2.value);
+})
+
+multiplybtn.addEventListener('click', function() {
+    finalResult.innerHTML = (+num1.value*+num2.value);
+})
